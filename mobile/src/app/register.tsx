57d724960a55328361300576d5ad3029484e1a65
Marco <marco@crabki.com>
2024-04-03 11:23:17 -0300

    feat: adicionar telas de cadastro e acesso

--- mobile/src/app/register.tsx
+++ mobile/src/app/register.tsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/input";
 import { Image, StatusBar, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
 import { Link } from "expo-router";
 
-export default function Home() {
+export default function Register() {
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -17,19 +17,27 @@ export default function Home() {
 
       <View className="w-full mt-12 gap-3">
         <Input>
-          <MaterialCommunityIcons
-            name="ticket-confirmation-outline"
+          <FontAwesome6
+            name="user-circle"
             size={20}
             color={colors.green[200]}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          <Input.Field placeholder="Nome completo" />
         </Input>
-        <Button title="Acessar credencial" />
+        <Input>
+          <MaterialIcons
+            name="alternate-email"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+        </Input>
+        <Button title="Realizar inscrição" />
         <Link
-          href="/register"
+          href="/"
           className="text-gray-100 text-base font-bold text-center mt-8"
         >
-          Ainda não possui ingresso?
+          Já possui ingresso?
         </Link>
       </View>
     </View>
